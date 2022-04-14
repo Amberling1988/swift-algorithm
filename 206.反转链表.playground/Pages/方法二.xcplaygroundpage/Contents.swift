fe1d7c042407283710f254834e7d7b0ebb9facbb
孟令令 <oraling@163.com>
2022-04-14 15:57:53 +0800

    练习 206.翻转链表

---
+++ 206.反转链表.playground/Pages/方法二.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+
+
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        
+        //       1 -> 2 -> 3 -> 4 -> 5 -> nil
+        // nil<- 1 <- 2<- 3 <- 4 <- 5
+        var preNode:ListNode?, curNode:ListNode?
+        curNode = head
+        
+        while curNode != nil {
+          
+            let nextNode = curNode?.next
+            
+            curNode?.next = preNode
+            preNode = curNode
+            curNode = nextNode
+        }
+        return preNode
+    }
+    
+}
